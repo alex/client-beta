de12acd600bf51b94a62437795481e060ef3e19d
Jack O'Connor <oconnor663@gmail.com>
2015-10-09 16:18:15 -0400

    get rid of `go get` now that we vendor

--- build.sh
+++ build.sh
@@ -9,11 +9,10 @@ rm -rf build/
 
 # Assemble the local GOPATH.
 export GOPATH="$(pwd)/build"
+export GO15VENDOREXPERIMENT=1
 mkdir -p "$GOPATH/src/github.com/keybase"
 cp -r client "$GOPATH/src/github.com/keybase"
 
 build_target="github.com/keybase/client/go/keybase"
-echo Fetching dependencies with go get...
-go get "$build_target"
 echo Building kbstage...
 go build -a -tags ./staging -o ./kbstage "$build_target"
